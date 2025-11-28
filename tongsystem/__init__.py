aeef89ad6319f9395971bbddeb5cb23f0611e824
Ryo Takizawa <64674886+crumbyRobotics@users.noreply.github.com>
2025-11-28 18:15:19 +0900

    Add error handling for device imports
    Handle ImportError for KeyboardManager and MouseManager

--- tongsystem/__init__.py
+++ tongsystem/__init__.py
@@ -1,4 +1,7 @@
 from .env import TongSystem, TongSystemFixedNeck, TongSystemUseOnlyLeftArm, TongSystemUseOnlyRightArm
 from .ikfastpy import ikfastpy
 from .kinematics import left_ee2world, right_ee2world, camera2left_ee, camera2right_ee, camera2left_ee_sim, camera2right_ee_sim
-from .device import KeyboardManager, MouseManager
+try:
+    from .device import KeyboardManager, MouseManager
+except ImportError as e:
+    print("KeyboardManager is not available because: " + str(e))
